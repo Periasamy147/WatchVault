2294c68d9c350e6df7735ce2bfac75a1f485f8e9
Periasamy147 <postbox1472@gmail.com>
2026-08-22 23:13:28 +0530

    Bump version to 1.8.0 for the design-system rebuild release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.watchvault"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7.0"
+        versionCode = 9
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
